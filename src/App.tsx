c09636be7dec58dc8195a6bb448291b559c0a29b
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-10-20 16:11:13 +0530

    app created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react';
+import './App.css'
+
+interface AppProps {}
+
+const App: FC<AppProps> = () => {
+  return (
+    <>
+    ECOM
+    </>
+  );
+}
+
+export default App;
